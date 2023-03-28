9bdc609b19299f140a654f8b5f265959e85a9213
JeremiNowikow <jeremi.nowikow@gmail.com>
2023-03-28 12:30:09 +0200

    Functions for seat management

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from pprint import pprint as pp
+
 class Flight:
     def __init__(self, flight_number, plane):
         self.flight_number = flight_number
@@ -6,7 +7,7 @@ class Flight:
 
         rows, letters = self.plane.get_seating_plan()
 
-        self.seats = [{letter: None for letter in letters} for _ in rows]
+        self.seats = [None] + [{letter: None for letter in letters} for _ in rows]
 
     def get_airways(self):
         return self.flight_number[:2]
@@ -17,8 +18,43 @@ class Flight:
     def get_airplane_model(self):
         return self.plane.get_model()
 
-    def allocate_passenger(self, seat="12C", passenger="Jarosław K."):
-        pass
+    def _parse_seat(self, seat="12C"):
+        rows, letters = self.plane.get_seating_plan()
+        letter = seat[-1]
+
+        if letter not in letters:
+            raise ValueError(f"Invalid seat letter: {letter}")
+        row_text = seat[:-1]
+
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError(f"Invalid row digit: {row_text}")
+
+        if row not in rows:
+            raise ValueError(f"Row is out of airplane range: {row}")
+
+        return row, letter
+
+    def allocate_passenger(self, seat, passenger):
+        row, letter = self._parse_seat(seat)
+
+        if self.seats[row][letter] is not None:
+            raise ValueError(f"Seat is already occupied: {seat}")
+        self.seats[row][letter] = passenger
+
+    def get_num_empty_seats(self):
+        # empty_seats = 0
+        # for row in self.seats:
+        #     if row is not None:
+        #         for passenger in row.values():
+        #             if passenger is None:
+        #                 empty_seats += 1
+        #
+        # return empty_seats
+
+        return sum(sum(1 for passenger in row.values() if passenger is None) for row in self.seats if row is not None)
+
 
 
 
@@ -52,5 +88,10 @@ airbus = AirbusA380()
 f = Flight("BA324", boeing)
 g = Flight("BA324", airbus)
 
+
+f.allocate_passenger("1C", "Jarosław K")
+f.allocate_passenger("12E", "Paweł K")
+f.allocate_passenger("12A", "Lech K")
 pp(f.seats)
-["a", "b"].sort(-1)
+print(f.plane.num_seats())
+print(f.get_num_empty_seats())
