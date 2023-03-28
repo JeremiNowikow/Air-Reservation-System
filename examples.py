b40478bc36c8c73502a29b942f297a3e2d2303d7
JeremiNowikow <jeremi.nowikow@gmail.com>
2023-03-28 11:09:19 +0200

    Revert ".gitignore fixed"
    This reverts commit 8af4b9a552222c765a919882d011dc8dadc6130d.

--- examples.py
+++ examples.py
@@ -1,9 +1,4 @@
-def my_max(*args):
-    temp_max = args[0]
-    for i in args:
-        if i > temp_max:
-            temp_max = i
-    return temp_max
-
-
-print(my_max(1,3,6,33,4,5))
+x = 1
+y = 2
+x, y = y, x
+print(x, y)
