8af4b9a552222c765a919882d011dc8dadc6130d
JeremiNowikow <jeremi.nowikow@gmail.com>
2023-03-28 10:56:10 +0200

    .gitignore fixed

--- examples.py
+++ examples.py
@@ -1,4 +1,9 @@
-x = 1
-y = 2
-x, y = y, x
-print(x, y)
+def my_max(*args):
+    temp_max = args[0]
+    for i in args:
+        if i > temp_max:
+            temp_max = i
+    return temp_max
+
+
+print(my_max(1,3,6,33,4,5))
